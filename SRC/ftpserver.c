a87fef331969782db4117ed0cc2d0ea79843bf0d
rajnitish <49732892+rajnitish@users.noreply.github.com>
2020-02-20 13:32:59 +0530

    First  build

---
+++ SRC/ftpserver.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+extern void RunCmd();
+
+extern char ipcmd[100];
+extern char opcmd[10000];
+extern int status;
+
+short SocketCreate(void)
+{
+    short hSocket;
+    printf("Create the socket\n");
+    hSocket = socket(AF_INET, SOCK_STREAM, 0);
+    return hSocket;
+}
+int BindCreatedSocket(int hSocket)
+{
+    int iRetval=-1;
+    int ClientPort = 90190;
+    struct sockaddr_in  remote= {0};
+    /* Internet address family */
+    remote.sin_family = AF_INET;
+    /* Any incoming interface */
+    remote.sin_addr.s_addr = htonl(INADDR_ANY);
+    remote.sin_port = htons(ClientPort); /* Local port */
+    iRetval = bind(hSocket,(struct sockaddr *)&remote,sizeof(remote));
+    return iRetval;
+}
+int main(int argc, char *argv[])
+{
+    int socket_desc, sock, clientLen, read_size;
+    struct sockaddr_in server, client;
+    char client_message[200]= {0};
+    char message[100] = {0};
+    const char *pMessage = "hello";
+    //Create socket
+    socket_desc = SocketCreate();
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+        return 1;
+    }
+    printf("Socket created\n");
+    //Bind
+    if( BindCreatedSocket(socket_desc) < 0)
+    {
+        //print the error message
+        perror("bind failed.");
+        return 1;
+    }
+    printf("bind done\n");
+    //Listen
+    listen(socket_desc, 3);
+    //Accept and incoming connection
+    while(1)
+    {
+        printf("Waiting for incoming connections...\n");
+        clientLen = sizeof(struct sockaddr_in);
+        //accept connection from an incoming client
+        sock = accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&clientLen);
+        if (sock < 0)
+        {
+            perror("accept failed");
+            return 1;
+        }
+        printf("Connection accepted\n");
+        memset(client_message, '\0', sizeof client_message);
+        memset(message, '\0', sizeof message);
+        //Receive a reply from the client
+        if( recv(sock, client_message, 200, 0) < 0)
+        {
+            printf("recv failed");
+            break;
+        }
+        printf("Client reply : %s\n",client_message);
+        if(strcmp("ls",client_message)==0)
+        {
+            memset(ipcmd,0,100);
+            sprintf(ipcmd,"%s","ls");
+            RunCmd();
+            strcpy(message,opcmd);
+            memset(opcmd,0,10000);
+            // Send data
+            if( send(sock, message, strlen(message), 0) < 0)
+            {
+                printf("Send failed");
+                return 1;
+            }
+        } 
+        else if(strcmp("cd",client_message)==0)
+        {
+            memset(ipcmd,0,100);
+            sprintf(ipcmd,"%s","cd");
+            RunCmd();
+            strcpy(message,opcmd);
+            memset(opcmd,0,10000);
+            // Send data
+            if( send(sock, message, strlen(message), 0) < 0)
+            {
+                printf("Send failed");
+                return 1;
+            }
+        } 
+        else if(strcmp("put",client_message)==0)
+        {
+            // separate put and file name
+            // open file, if file not present create file
+            //recieve data from client and write to file
+        } 
+        else if(strcmp("get",client_message)==0)
+        {
+            // separate put and file name
+            // open file, if file not present send error msg to client
+            // send data from server to client to write in a file
+        } else
+        {
+            strcpy(message,"Invalid cmd mps !");
+        }
+        
+        
+        
+        close(sock);
+        sleep(1);
+    }
+    return 0;
+}
---
+++ SRC/runcmd.c
@@ -0,0 +1,28 @@
+#include<string.h>
+#include<stdio.h>
+char ipcmd[100]={0};
+char opcmd[10000]={0};
+FILE *fp;
+int status;
+
+void RunCmd()
+{
+
+char path[4096];
+//sprintf(ipcmd,"%s","ls");
+
+fp = popen(ipcmd, "r");
+if (fp == NULL){
+   printf("Err:cmd Not executed %s",ipcmd);
+}
+
+
+while (fgets(path, sizeof(path), fp) != NULL){
+    //printf("%s", opcmd);
+    strcat(opcmd,path);
+}
+printf("%s", opcmd);
+status = pclose(fp);
+ //sleep(1);
+ return;
+}
